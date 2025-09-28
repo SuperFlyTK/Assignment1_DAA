a2153d74f18bebf47d757dd25ed69363801340fd
Yerdaulet Akhanov <rnesitajlo@gmail.com>
2025-09-28 23:51:11 +0500

    Update Metrics.java

--- src/main/java/org/example/Metrics.java
+++ src/main/java/org/example/Metrics.java
@@ -3,13 +3,16 @@ package org.example;
 public class Metrics {
     private int comparisons;
     private int swaps;
+    private int currentDepth;
+    private int maxDepth;
 
     public void reset() {
         comparisons = 0;
         swaps = 0;
+        currentDepth = 0;
+        maxDepth = 0;
     }
 
-    // Методы вызывающие MergeSort
     public void incrementComparisons() {
         comparisons++;
     }
@@ -18,11 +21,26 @@ public class Metrics {
         swaps++;
     }
 
+    public void enterRecursion() {
+        currentDepth++;
+        if (currentDepth > maxDepth) {
+            maxDepth = currentDepth;
+        }
+    }
+
+    public void exitRecursion() {
+        currentDepth--;
+    }
+
     public int getComparisons() {
         return comparisons;
     }
 
     public int getSwaps() {
         return swaps;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
     }
 }
